1ca29183c536655a4312b7199ad0708a1326d809
Manoj Saxena <msaxena2024@gmail.com>
2024-03-19 13:58:44 +0000

    swagger for auction items

--- src/auction-items/auction-items.controller.ts
+++ src/auction-items/auction-items.controller.ts
@@ -1,22 +1,46 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { AuctionItemsService } from './auction-items.service';
 import { CreateAuctionItemDto } from './dto/create-auction-item.dto';
+import {
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
 @Controller('auctionItems')
+@ApiTags('Auction Items')
 export class AuctionItemsController {
   constructor(private readonly auctionItemsService: AuctionItemsService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Adds an entry for auction' })
+  @ApiResponse({
+    status: 200,
+    description: 'sample-auction-app is healthy!',
+    type: String,
+  })
   create(@Body() createAuctionItemDto: CreateAuctionItemDto) {
     return this.auctionItemsService.create(createAuctionItemDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'returns all auctionItems' })
+  @ApiResponse({
+    status: 200,
+    description: 'sample-auction-app is healthy!',
+    type: String,
+  })
   findAll() {
     return this.auctionItemsService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'returns a specific auctionItem' })
+  @ApiResponse({
+    status: 200,
+    description: 'sample-auction-app is healthy!',
+    type: String,
+  })
   findOne(@Param('id') id: string) {
     return this.auctionItemsService.findOne(+id);
   }
--- src/healthcheck/healthcheck.controller.ts
+++ src/healthcheck/healthcheck.controller.ts
@@ -1,11 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
 import { HealthcheckService } from './healthcheck.service';
+import {
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Health check')
 @Controller('healthcheck')
 export class HealthcheckController {
   constructor(private readonly healthcheckService: HealthcheckService) {}
 
   @Get()
+  @ApiOperation({ summary: 'check application health' })
+  @ApiResponse({
+    status: 200,
+    description: 'sample-auction-app is healthy!',
+    type: String,
+  })
   healthCheck() {
     return this.healthcheckService.healthCheck();
   }
