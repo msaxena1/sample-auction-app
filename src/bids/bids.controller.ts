0f6bc11764dddd0a2349905359df6d01418355a0
Manoj Saxena <msaxena2024@gmail.com>
2024-03-20 20:34:53 +0000

    logging audit to console under Bid-Audit category

--- src/bids/bids.controller.ts
+++ src/bids/bids.controller.ts
@@ -1,16 +1,10 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
 import { BidsService } from './bids.service';
 import { CreateBidDto } from './dto/create-bid.dto';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { HttpException, HttpStatus } from '@nestjs/common';
+import { AUDIT_CATEGORY } from './constants';
 
 @Controller('bids')
 @ApiTags('Bids')
@@ -25,10 +19,18 @@ export class BidsController {
     type: String,
   })
   async create(@Body() createBidDto: CreateBidDto): Promise<any> {
+    const incomingMsg = `Incoming Audit Req: ${JSON.stringify(createBidDto)}`;
+    Logger.log(incomingMsg, AUDIT_CATEGORY);
     try {
       const res = await this.bidsService.create(createBidDto);
+
+      const outgoingMsg = `Outgoing Audit Res: ${JSON.stringify(res)}`;
+      Logger.log(outgoingMsg, AUDIT_CATEGORY);
+
       return res;
     } catch (error) {
+      const outgoingMsg = `Outgoing Audit Res: Exception: (${error}) for Req: ${JSON.stringify(createBidDto)}`;
+      Logger.error(outgoingMsg, AUDIT_CATEGORY);
       throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
---
+++ src/bids/constants.ts
@@ -0,0 +1,1 @@
+export const AUDIT_CATEGORY = 'Bid-Audit';
